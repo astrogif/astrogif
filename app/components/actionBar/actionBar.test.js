ca7cc652e9d2071b054d7668d4c5a77978a45120
James Macfie <james@macfie.co.nz>
2017-01-31 21:23:05 +1300

    Add a couple more tests for ActionBar

--- app/components/actionBar/actionBar.test.js
+++ app/components/actionBar/actionBar.test.js
@@ -1,7 +1,9 @@
 /* eslint-disable no-unused-expressions */
 import React from 'react';
+import electron from 'electron';
 import { shallow } from 'enzyme';
 import { expect } from 'chai';
+import sinon from 'sinon';
 import ActionBar from './actionBar';
 
 const defaultProps = {
@@ -32,25 +34,45 @@ describe('<ActionBar />', () => {
   });
 
   describe('Github icon', () => {
+    before(() => {
+      sinon.stub(electron.shell, 'openExternal').returns(null);
+    });
+
+    after(() => {
+      electron.shell.openExternal.restore();
+    });
+
     it('renders', () => {
       const cmp = shallow(actionBarFactoy());
       expect(cmp.find('.qa-github')).to.have.length(1);
     });
 
     it('Navigates away to the github repo', () => {
-
+      const cmp = shallow(actionBarFactoy());
+      cmp.find('.qa-github').simulate('click');
+      expect(electron.shell.openExternal.calledOnce).to.be.true;
     });
   });
 
   describe('Quit icon', () => {
+    let hasQuit = false;
+    before(() => {
+      electron.remote.app.quit = () => { hasQuit = true; };
+    });
+
+    after(() => {
+      electron.remote.app.quit = undefined;
+    });
+
     it('renders', () => {
       const cmp = shallow(actionBarFactoy());
       expect(cmp.find('.qa-quit')).to.have.length(1);
     });
 
     it('quits the app', () => {
-
+      const cmp = shallow(actionBarFactoy());
+      cmp.find('.qa-quit').simulate('click');
+      expect(hasQuit).to.be.true;
     });
   });
-
 });
