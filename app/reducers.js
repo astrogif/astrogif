c267f8b92a3890ee44a96e815f32bc2e24f88572
James Macfie <james@macfie.co.nz>
2017-02-10 10:35:51 +1300

    Muppet. Reference the right reducer

--- app/reducers.js
+++ app/reducers.js
@@ -4,7 +4,7 @@ import app from './containers/app/reducer';
 import gif from './containers/gif/reducer';
 import search from './containers/search/reducer';
 import settings from './containers/settings/reducer';
-import updater from './containers/settings/reducer';
+import updater from './containers/updater/reducer';
 
 const rootReducer = combineReducers({
   app,
