a34e73e6bf5cdbcd304a46d260a735643e8ca377
James Macfie <james@macfie.co.nz>
2017-02-10 21:25:20 +1300

    Add Github logo SVG

--- app/components/actionBar/actionBar.jsx
+++ app/components/actionBar/actionBar.jsx
@@ -1,9 +1,8 @@
 import React, { PropTypes } from 'react';
 import cn from 'classnames';
 import { shell, remote } from 'electron';
-import { Power, Settings } from '../icons/icons';
+import { Github, Power, Settings } from '../icons/icons';
 import Updater from '../../containers/updater';
-import github from './github.png';
 import styles from './styles.css';
 
 const openRepo = () => { shell.openExternal('https://github.com/astrogif/astrogif'); };
@@ -13,7 +12,9 @@ const ActionBar = ({ changePage }) => <div>
   <button alt="Settings" className={cn('qa-settings', styles.button)} onClick={() => { changePage('settings'); }}>
     <Settings className={styles.cog} />
   </button>
-  <img alt="Open on Github" src={github} className={cn('qa-github', styles.github)} onClick={openRepo} />
+  <button alt="Github" className={cn('qa--github', styles.button)} onClick={openRepo}>
+    <Github className={styles.github} />
+  </button>
   <button alt="Quit" className={cn('qa-quit', styles.button)} onClick={quit}>
     <Power className={styles.power} />
   </button>
