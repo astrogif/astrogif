837f89ccc3a7a79d75f8fdf321b0780550612ba4
James Macfie <james@macfie.co.nz>
2017-02-10 21:51:47 +1300

    Adding down arrow SVG

--- app/components/select/select.jsx
+++ app/components/select/select.jsx
@@ -1,14 +1,13 @@
 import React, { PropTypes } from 'react';
 import styles from './styles.css';
-import down from './down.png';
+import { Down } from '../icons/icons';
 
 const Select = ({ children, onChange, value }) => (<div className={styles.container}>
   <select className={styles.select} onChange={onChange} value={value}>
     {children}
   </select>
-  <img className={styles.down} src={down} alt="down" />
+  <Down className={styles.down} height="15" width="15" />
 </div>);
-
 
 Select.propTypes = {
   children: PropTypes.any,
