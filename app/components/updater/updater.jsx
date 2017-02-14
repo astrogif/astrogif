06a346daee7455c2255df6c4070a081900467247
James Macfie <james@macfie.co.nz>
2017-02-14 18:06:14 +1300

    Update as SVG glyph + test svg fix

--- app/components/updater/updater.jsx
+++ app/components/updater/updater.jsx
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import { Update } from '../icons/icons';
+import Icon from '../icons/icons';
 import styles from './styles.css';
 
 export default class Updater extends Component {
@@ -8,7 +8,7 @@ export default class Updater extends Component {
 
     if (available && downloaded) {
       return (<button className={styles.container} onClick={installAndRestart} >
-        <Update height={20} width={20} />
+        <Icon glyph="update" height={20} width={20} />
         <span className={styles.text}>New version!</span>
       </button>);
     }
